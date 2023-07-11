0454dbe73d7e9b722517d442580b31ace2059ae3
Ronilsonb2 <ronilson-b2@hotmail.com>
2023-07-10 23:44:24 -0300

    Game seed

--- src/main/java/com/br/dslist/entities/Game.java
+++ src/main/java/com/br/dslist/entities/Game.java
@@ -12,25 +12,31 @@ public class Game {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
-
     @Column(name = "game_year")
     private String year;
     private String genre;
     private String platforms;
+
+    private String score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game() {
 
     }
 
-    public Game(Long id, String title, String year, String genre, String platforms, String imgUrl, String shortDescription, String longDescription) {
+    public Game(Long id, String title, String year, String genre, String platforms, String score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
         this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
@@ -74,6 +80,14 @@ public class Game {
 
     public void setPlatforms(String platforms) {
         this.platforms = platforms;
+    }
+
+    public String getScore() {
+        return score;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
     }
 
     public String getImgUrl() {
